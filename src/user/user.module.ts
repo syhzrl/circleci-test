f8efd89d954f31e14f3ef3bcb5f1e83ffa6fa2a4
syhzrl <syhzrl@gmail.com>
2022-12-05 15:28:46 +0800

    test init

--- src/user/user.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
-import { UserService } from './user.service';
-import { UserResolver } from './user.resolver';
-
-@Module({
-    providers: [UserService, UserResolver, PrismaService],
-})
-export class UserModule { }
