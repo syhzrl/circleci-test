83f1439402fd44febba55c3566c2a1d1b2069216
syhzrl <37472652+syhzrl@users.noreply.github.com>
2022-12-05 15:42:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-    res.write('ITS WORKING');
+    res.write('ITS WORKING 2');
     res.end();
 }).listen(8080, '0.0.0.0'); 
